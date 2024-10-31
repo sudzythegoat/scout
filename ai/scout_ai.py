24e93f23a07ada02669f4d2720d05660d2e558e8
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-31 09:39:22 -0500

    Update scout_ai.py

--- ai/scout_ai.py
+++ ai/scout_ai.py
@@ -1,1 +1,12 @@
-#
+from g4f.client import Client
+def ai_search(alias):
+    prompt = f"Tell me everything you know about {alias}, if you do not know anything please say 'NFOUND'"
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    gpt_response = str(response.choices[0].message.content)
+    if not "NFOUND" in gpt_response:
+        found_info = gpt_response
+    else:
+        found_info = "N/A"
