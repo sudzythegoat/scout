aa5784411c6f75ef8c0b635c317dca036e0b0027
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-03 15:33:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from ip.scoutip import ipinfo
 from ai.scout_ai import ai_search
 from ai.scout_ai import found_info
 from search.gsearch import search_google 
+from phone.lookup import phonenum_lookup
