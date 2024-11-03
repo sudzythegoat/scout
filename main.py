d5e30bdbcab327d12b983f333851ffa4e0d8915a
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-03 15:42:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ from search.gsearch import search_google
 from phone.lookup import phonenum_lookup
 from user.usercheck import find_username
 from cfg import title, version
+from colorama import style, fore
